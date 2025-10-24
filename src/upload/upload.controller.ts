58be450fd63c53c67aa73c9d75bb73792bac7ca5
Khang Quách <khangB2203505@student.ctu.edu.vn>
2025-10-25 05:14:27 +0700

    Delete routes upload controller

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -7,28 +7,4 @@ import { UpdateUploadDto } from './dto/update-upload.dto';
 export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
 
-  @Post()
-  create(@Body() createUploadDto: CreateUploadDto) {
-    return this.uploadService.create(createUploadDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.uploadService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.uploadService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUploadDto: UpdateUploadDto) {
-    return this.uploadService.update(+id, updateUploadDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.uploadService.remove(+id);
-  }
 }
