d40d92635871b38f282559e4b54bd76e3625e649
KhangQuach0405 <quachthieukhang@gmail.com>
2025-11-19 23:53:10 +0700

    authorization for service-packages module

--- src/service-packages/service-packages.controller.ts
+++ src/service-packages/service-packages.controller.ts
@@ -1,12 +1,18 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query, UseGuards } from '@nestjs/common';
 import { ServicePackagesService } from './service-packages.service';
 import { CreateServicePackageDto } from './dto/create-service-package.dto';
 import { UpdateServicePackageDto } from './dto/update-service-package.dto';
+import { JwtAuthGuard } from '@auth/guards/jwt-auth.guard';
+import { RolesGuard } from '@auth/guards/roles.guard';
+import { Roles } from '@auth/decorators/roles.decorator';
+import { UserRole } from '@users/schemas/user.schema';
 
 @Controller('service-packages')
 export class ServicePackagesController {
   constructor(private readonly svc: ServicePackagesService) { }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.Admin, UserRole.Staff)
   @Post()
   create(@Body() dto: CreateServicePackageDto) { return this.svc.create(dto); }
 
@@ -18,9 +24,13 @@ export class ServicePackagesController {
   @Get(':id')
   get(@Param('id') id: string) { return this.svc.findOne(id); }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.Admin, UserRole.Staff)
   @Put(':id')
   update(@Param('id') id: string, @Body() dto: UpdateServicePackageDto) { return this.svc.update(id, dto); }
   
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.Admin, UserRole.Staff)
   @Delete(':id')
   del(@Param('id') id: string) { return this.svc.remove(id); }
 }
